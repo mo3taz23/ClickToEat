f5e1902be2bf37f1dab9d81f65b004205070cc1b
moataz lasheen <61991975+mo3taz23@users.noreply.github.com>
2021-11-29 14:10:21 +0200

    Add files via upload

---
+++ models/user.dart
@@ -0,0 +1,15 @@
+class User {
+  String name;
+  String password;
+  String email;
+  String userName;
+  int phoneNumber;
+
+  User(
+    this.name,
+    this.password,
+    this.email,
+    this.userName,
+    this.phoneNumber,
+  );
+}
